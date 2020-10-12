f32ca3012b33d4655e71a869406431d82e3a5300
Nikko Parsons <51349845+Jenyne@users.noreply.github.com>
2020-10-11 18:40:47 -0600

    Working on Session Setup. Fixed NullPointerException....

--- MyLogin/src/java/Servlets/LoginServlet.java
+++ MyLogin/src/java/Servlets/LoginServlet.java
@@ -24,16 +24,19 @@ import javax.servlet.http.HttpSession;
 @WebServlet(name = "LoginServlet", urlPatterns = {"/Login"})
 public class LoginServlet extends HttpServlet {
 
+    String userloginname;
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         HttpSession session = request.getSession(false);
-        if (session!= null) {
+        if (session != null) {
+            session.getAttribute("user");
             getServletContext().getRequestDispatcher("/WEB-INF/home.jsp")
                     .forward(request, response);
         } else {
             session = request.getSession(true);
-            session.setAttribute("user",null);
+            session.setAttribute("user", null);
             getServletContext().getRequestDispatcher("/WEB-INF/login.jsp")
                     .forward(request, response);
         }
@@ -43,33 +46,44 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        HttpSession session = null;
-        AccountService ac = new AccountService();
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
-        User user = ac.login(username, password);
-        // pass user/password to accserv.login(). If user = user then do.
-        if (user != null) {
-            String curuser = user.getUsername();
-            if (curuser == "adam" || curuser == "betty") {
-                String capuser = curuser.substring(0, 1).toUpperCase() + curuser.substring(1);
-                request.setAttribute("User", capuser);
+        try {
+            HttpSession session = request.getSession(false);
 
-                //response.sendRedirect(request.getContextPath() + "/home");
-                getServletContext().getRequestDispatcher("/WEB-INF/home.jsp")
+            AccountService ac = new AccountService();
+            String username = request.getParameter("username");
+            String password = request.getParameter("password");
+            User user = ac.login(username, password);
+            if (user != null) {
+                String curuser = user.getUsername();
+                if (curuser == null || curuser == "") {
+                    getServletContext().getRequestDispatcher("/WEB-INF/login.jsp")
+                            .forward(request, response);
+                }
+                if (curuser == "adam" || curuser == "betty") {
+                    String capuser = curuser.substring(0, 1).toUpperCase() + curuser.substring(1);
+                    request.setAttribute("user", capuser);
+                    session.setAttribute("userloginname", capuser);
+                    //response.sendRedirect(request.getContextPath() + "/home");
+                    getServletContext().getRequestDispatcher("/WEB-INF/home.jsp")
+                            .forward(request, response);
+                } else if (curuser == "admin") {
+                    String capuser = curuser.substring(0, 1).toUpperCase() + curuser.substring(1);
+                    request.setAttribute("user", capuser);
+                    session.setAttribute("userloginname", curuser);
+                }
+                session = request.getSession();
+                session.setAttribute("userloginname", user);
+            } else {
+                String curuser = request.getParameter("username");
+                request.setAttribute("username", curuser);
+                getServletContext().getRequestDispatcher("/WEB-INF/login.jsp")
                         .forward(request, response);
-            } else if (curuser == "admin") {
+
             }
-            session = request.getSession();
-            session.setAttribute("user", user);
-        } else {
-            String curuser = request.getParameter("username");
-            request.setAttribute("username", curuser);
+        }catch (NullPointerException e){
             getServletContext().getRequestDispatcher("/WEB-INF/login.jsp")
-                    .forward(request, response);
-
+                        .forward(request, response);
         }
-
     }
 
     @Override
@@ -79,7 +93,7 @@ public class LoginServlet extends HttpServlet {
 
     public static void logout(HttpServletRequest request, HttpServletResponse response) {
         HttpSession session = request.getSession();
-        if (session!= null) {
+        if (session != null) {
             session.invalidate();
 
         }
